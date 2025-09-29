7f210e56c69f8a2ed82ea3c7d1955d28e6fea3d7
Maria Cabrera <mariacabrera03@gmail.com>
2025-09-29 14:33:36 -0300

    Se agrega animacion de carga al iniciar la pagina

---
+++ TP2/js/carousel.js
@@ -0,0 +1,27 @@
+// const url = "https://vj.interfaces.jima.com.ar/api/v2";
+
+// let carrusel = document.querySelector("#carousel-container");
+
+
+// fetch(url)
+//     .then(response => response.json())
+//     .then(games => {
+//         console.log('Juegos disponibles:', games);
+//         // Procesar la lista de juegos con campos adicionales
+//         games.forEach(game => {
+//             console.log(`${game.name} - Rating: ${game.rating}`);
+//             console.log(`Descripción: ${game.description}`);
+//             console.log(`Imagen optimizada: ${game.background_image_low_res}`);
+//         });
+//     })
+//     .catch(error => {
+//         console.error('Error al obtener los juegos:', error);
+//     });
+
+//     async function cargarJuegos() {
+//         carrusel.innerHTML =
+//          `<div class="carousel">
+         
+//         `
+        
+//     }
---
+++ TP2/js/lodMain.js
@@ -0,0 +1,44 @@
+"use strict";
+
+const divLoadWindow = document.createElement("div");
+divLoadWindow.id = "loadWindow";
+
+const divLoadContent = document.createElement("div");
+divLoadContent.id = "loadContent";
+
+const divLogoLoad = document.createElement("div");
+divLogoLoad.id = "logoLoad";
+
+const imgLogoLoad = document.createElement("img");
+imgLogoLoad.src = "img/icons/logoCarga.jpg";
+divLogoLoad.appendChild(imgLogoLoad);
+
+const pLoad = document.createElement("p");
+pLoad.id = "pLoad";
+
+document.body.appendChild(divLoadWindow);
+divLoadWindow.appendChild(divLoadContent);
+divLoadContent.appendChild(divLogoLoad);
+divLoadContent.appendChild(pLoad);
+
+let count = 0;
+const total = 100; // Simulated total loading steps
+const duration = 5000; // Total duration of the loading in milliseconds
+const intervalTime = duration / total; // Time between each step
+document.body.classList.add("noScroll");
+
+const interval = setInterval(() => {
+    count++;
+    pLoad.innerHTML = Math.round(count) + "%";
+
+    if (count >= total) {
+        clearInterval(intervalTime);
+        pLoad.innerHTML = "100%";
+        divLoadContent.style.animation = "pulseRotate 1s ease-in-out forwards";
+        divLoadWindow.classList.add("finalize");
+        setTimeout(() => {
+            document.body.classList.remove("noScroll");
+            divLoadWindow.style.display = "none";
+        }, 2000); // Match the duration of the pulseRotate animation
+    }
+}, intervalTime);
