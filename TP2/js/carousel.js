26764e19078431eaf68b362635b169e59e9a5b82
Maria Cabrera <mariacabrera03@gmail.com>
2025-09-30 12:53:59 -0300

    Se agrega consumo de la api de juegos

--- TP2/js/carousel.js
+++ TP2/js/carousel.js
@@ -1,27 +1,49 @@
-// const url = "https://vj.interfaces.jima.com.ar/api/v2";
-
-// let carrusel = document.querySelector("#carousel-container");
+"use strict";
 
 
-// fetch(url)
-//     .then(response => response.json())
-//     .then(games => {
-//         console.log('Juegos disponibles:', games);
-//         // Procesar la lista de juegos con campos adicionales
-//         games.forEach(game => {
-//             console.log(`${game.name} - Rating: ${game.rating}`);
-//             console.log(`Descripción: ${game.description}`);
-//             console.log(`Imagen optimizada: ${game.background_image_low_res}`);
-//         });
-//     })
-//     .catch(error => {
-//         console.error('Error al obtener los juegos:', error);
-//     });
+document.addEventListener("DOMContentLoaded", startPage);
 
-//     async function cargarJuegos() {
-//         carrusel.innerHTML =
-//          `<div class="carousel">
-         
-//         `
+
+function startPage(){
+    const url = "https://vj.interfaces.jima.com.ar/api/v2";
+    
+    let carrusel = document.querySelector("#carousel-container");
+    
+    async function showInfo(){
+        let saveJson = await fetch(url);
+        let carruselData = await saveJson.json();
+        for(let card of carruselData){
+            console.log(card.id)
+        }
         
-//     }
+        
+    }
+    showInfo();
+    // fetch(url)
+    //     .then(response => response.json())
+    //     .then(games => {
+        //         console.log('Juegos disponibles:', games);
+        //         // Procesar la lista de juegos con campos adicionales
+        //         games.forEach(game => {
+            //             console.log(`${game.name} - Rating: ${game.rating}`);
+            //             console.log(`Descripción: ${game.description}`);
+            //             console.log(`Imagen optimizada: ${game.background_image_low_res}`);
+            //         });
+            //     })
+            //     .catch(error => {
+                //         console.error('Error al obtener los juegos:', error);
+    //     });
+    
+    //     async function cargarJuegos() {
+        //         carrusel.innerHTML =
+        //          `<div class="carousel">
+        
+        //         `
+        
+        // }
+    }
+    
+    // <div class="category-container">
+    //     <h3>Porque jugaste a Monopolis</h3>
+    //     <div class="carousel"></div>
+    // </div>
