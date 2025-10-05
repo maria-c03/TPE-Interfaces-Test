a8818211f47d3bc7beee2a8820b2e14d4c533503
Martin <Martinrbustamante01@gmail.com>
2025-10-05 20:39:24 -0300

    Cartel comentario terminado

---
+++ TP2/js/share-containerDrop.js
@@ -0,0 +1,18 @@
+
+"use strict"
+
+document.addEventListener('DOMContentLoaded', ()=>{
+    const btnShare = document.querySelector('.btn-share');
+    const btnCancel = document.querySelector('.fa-xmark');
+    const shareBox = document.querySelector('.share-container');
+    
+    btnShare.addEventListener('click',()=>{
+        shareBox.classList.toggle('active');
+    })
+    document.addEventListener('click', (e)=>{
+        if(!shareBox.contains(e.target) && !btnShare.contains(e.target)
+             || btnCancel.contains(e.target)){
+                shareBox.classList.remove('active');
+        }
+    })
+})
