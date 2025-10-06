c5ca4da56835e0e41bc8bfb0d0c60e46e297824d
Maria Cabrera <mariacabrera03@gmail.com>
2025-10-06 20:27:24 -0300

    Se agrega animacion registro exitoso

--- TP2/js/toggleLogin.js
+++ TP2/js/toggleLogin.js
@@ -7,11 +7,11 @@ const btnLogin = document.getElementById("btn-login");
 const btnRegister = document.getElementById("btn-registration");
 
 btnRegister.addEventListener("click", () => {
-  loginContainer.classList.add("oculto");
-  registerContainer.classList.remove("oculto");
+  loginContainer.classList.add("hide");
+  registerContainer.classList.remove("hide");
 });
 
 btnLogin.addEventListener("click", () => {
-  registerContainer.classList.add("oculto");
-  loginContainer.classList.remove("oculto");
+  registerContainer.classList.add("hide");
+  loginContainer.classList.remove("hide");
 });
