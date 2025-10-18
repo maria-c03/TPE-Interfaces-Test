977247f97d9cfe6ce88299b9a2cc4fd034f9daa5
Maria Cabrera <mariacabrera03@gmail.com>
2025-10-17 21:53:40 -0300

    Se crea un unico boton para evitar errores de evento

--- TP3/js/blockaJs.js
+++ TP3/js/blockaJs.js
@@ -4,6 +4,10 @@ document.addEventListener("DOMContentLoaded", function () {
     const ctx = canvas.getContext("2d");
     const width = canvas.width;
     const height = canvas.height;
+
+    const backgroundImage = new Image();
+    backgroundImage.src = "img/imgJuegos/marvel_blocka.png";
+
     //array con la ruta de las imagenes del juego
     const images = [
         "img/imgBlocka/image1.png",
@@ -14,19 +18,13 @@ document.addEventListener("DOMContentLoaded", function () {
         "img/imgBlocka/image6.jpg"
     ];
     //--- botón jugar ---
-    let playBtn = "Jugar de nuevo";
-    const playButton = {
+    const button = {
         x: 731,
         y: 325,
         width: 100,
-        height: 100
-    };
-
-    const playAgain = {
-        x: 680,
-        y: 600,
-        width: 200,
-        height: 50
+        height: 100,
+        shape: "circle",
+        text: "Jugar de nuevo"
     };
 
     const thumbnails = [];
@@ -62,8 +60,23 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     });
 
-    // --- interfaz de usuario ---
+    // --------- INTERFAZ DE USUARIO ----------
     function drawUI() {
+        //si el juego no esta activo, dibujo la imagen de fondo y el boton de jugar
+        if (!gameStarted) {
+            // Si la imagen de fondo ya cargó
+            if (backgroundImage.complete) {
+                ctx.drawImage(backgroundImage, 0, 0, width, height);
+            } else {
+                // Si todavía no cargó, pintamos un fondo negro mientras tanto
+                ctx.fillStyle = "#000000";
+                ctx.fillRect(0, 0, width, height);
+                // Cuando la imagen termine de cargar, redibujamos automáticamente
+                backgroundImage.onload = () => drawUI();
+            }
+            drawButton();
+            return;
+        }
         // Fondo del canva
         ctx.fillStyle = "#000000";
         ctx.fillRect(0, 0, width, height);
@@ -72,15 +85,11 @@ document.addEventListener("DOMContentLoaded", function () {
         thumbnails.forEach((thumb) => {
             ctx.drawImage(thumb.image, thumb.x, thumb.y, thumb.width, thumb.height);
         });
-        //si el juego no esta activo, dibujo el boton de jugar
-        if (!gameStarted) {
-            drawBtnPlayStart();
-        }
         //muestro el tiempo de juego
         drawTimer();
     }
 
-    // --- logica ---
+    // --------- FUNCIONES LOGICA Y DIBUJO ---------
 
     //devuelvo una rotacion aleatoria
     function getRandomRotation() {
@@ -122,9 +131,6 @@ document.addEventListener("DOMContentLoaded", function () {
                 piece.width,
                 piece.height //tamaño con que se dibuja.
             );
-            // Luego aplica el filtro sobre la región ya dibujada
-            aplicarFiltro(ctx, piece.canvasX, piece.canvasY, piece.width, piece.height);
-            console.log("pX: " + piece.sourceX, "pY: " + piece.sourceY);
             //vuelvo al estado original del contexto (antes de translate/rotate).
             ctx.restore();
         });//cada pieza aparece en su posición con rotación propia
@@ -135,43 +141,43 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     }
 
-    // --- funciones ---
-    function drawBtnPlayStart() {
+    function drawButton() {
         // circulo
-        ctx.beginPath();
-        const centerX = playButton.x + playButton.width / 2;
-        const centerY = playButton.y + playButton.height / 2;
-        const radius = Math.min(playButton.width, playButton.height) / 2;
+        if (button.shape === "circle") {
+            ctx.beginPath();
+            const centerX = button.x + button.width / 2;
+            const centerY = button.y + button.height / 2;
+            const radius = Math.min(button.width, button.height) / 2;
 
-        ctx.arc(centerX, centerY, radius, 0, Math.PI * 2);
-        ctx.fillStyle = "#F72585";
-        ctx.fill();
+            ctx.arc(centerX, centerY, radius, 0, Math.PI * 2);
+            ctx.fillStyle = "#F72585";
+            ctx.fill();
 
-        // --- Triángulo ---
-        ctx.beginPath();
-        ctx.lineJoin = "round";   // une las líneas con un borde redondeado
-        ctx.lineCap = "round";    // redondea los extremos de cada línea
-        // vértices del triángulo
-        const size = radius * 0.4; // proporción del tamaño del triángulo
-        ctx.moveTo(centerX - size / 2, centerY - size / 1.5); // punta superior izquierda
-        ctx.lineTo(centerX - size / 2, centerY + size / 1.5); // punta inferior izquierda
-        ctx.lineTo(centerX + size / 1.5, centerY); // punta derecha
-        ctx.closePath();
+            // --- Triángulo ---
+            ctx.beginPath();
+            ctx.lineJoin = "round";   // une las líneas con un borde redondeado
+            ctx.lineCap = "round";    // redondea los extremos de cada línea
+            // vértices del triángulo
+            const size = radius * 0.4; // proporción del tamaño del triángulo
+            ctx.moveTo(centerX - size / 2, centerY - size / 1.5); // punta superior izquierda
+            ctx.lineTo(centerX - size / 2, centerY + size / 1.5); // punta inferior izquierda
+            ctx.lineTo(centerX + size / 1.5, centerY); // punta derecha
+            ctx.closePath();
 
-        ctx.fillStyle = "white";
-        ctx.strokeStyle = "white";
-        ctx.lineWidth = 6; // el grosor da suavidad
-        ctx.stroke();
-        ctx.fill();
-    }
-    function drawBtnPlay() {
-        // boton volver a jugar
-        ctx.fillStyle = "#F72585";
-        ctx.fillRect(playAgain.x, playAgain.y, playAgain.width, playAgain.height);
-        ctx.fillStyle = "white";
-        ctx.font = "18px Roboto";
-        ctx.textAlign = "center";
-        ctx.fillText(playBtn, playAgain.x + playAgain.width / 2, playAgain.y + 30);
+            ctx.fillStyle = "white";
+            ctx.strokeStyle = "white";
+            ctx.lineWidth = 6; // el grosor da suavidad
+            ctx.stroke();
+            ctx.fill();
+        } else if (button.shape === "rect") {
+            // boton volver a jugar
+            ctx.fillStyle = "#F72585";
+            ctx.fillRect(button.x, button.y, button.width, button.height);
+            ctx.fillStyle = "white";
+            ctx.font = "18px Roboto";
+            ctx.textAlign = "center";
+            ctx.fillText(button.text, button.x + button.width / 2, button.y + 30);
+        }
     }
 
     function drawTimer() {
@@ -186,133 +192,229 @@ document.addEventListener("DOMContentLoaded", function () {
         // mensaje en pantalla
         clearInterval(timerInterval); //detiene el contador por segundos.
         gameStarted = false; //marco que termino el juego
-       
+
         ctx.fillStyle = "lime";
         ctx.font = "30px Roboto";
         ctx.textAlign = "center";
         ctx.fillText("¡Puzzle resuelto!", 775, 320);
-        //pinto el boton jugar de nuevo 
-        drawBtnPlay();
+
+        // Cambiar botón a rectángulo
+        button.shape = "rect";
+        button.x = 680;
+        button.y = 600;
+        button.width = 200;
+        button.height = 50;
+
+        drawButton();
     }
+
+    // -------------PIEZAS--------------
+
+    function createPice() {
+        pieces = [];
+        //calculo el ancho y alto de cada pieza
+        const pieceWidth = imageActual.width / cols;
+        const pieceHeight = imageActual.height / rows;
+        //recorro filas y columnas
+        for (let y = 0; y < rows; y++) {
+            for (let x = 0; x < cols; x++) {
+                const canvasX = posStartX + x * pieceWidth;
+                const canvasY = posStartY + y * pieceHeight;
+                //por cada celda agrego un objeto pieza con sus coordenadas, tamaño, posicion en el canvas y rotacion.
+                pieces.push({
+                    sourceX: x * pieceWidth,
+                    sourceY: y * pieceHeight,
+                    sourceWidth: pieceWidth,
+                    sourceHeight: pieceHeight,
+                    width: pieceWidth,
+                    height: pieceHeight,
+                    canvasX,
+                    canvasY,
+                    rotation: getRandomRotation()
+                });
+            }
+        }
+        //marco el inicio del juego en true y reseteo el tiempo en cero
+        gameStarted = true;
+        time = 0;
+        //si ya habia un timerInterval lo limpio
+        if (timerInterval) {
+            clearInterval(timerInterval);
+        }
+        //creo un nuevo timerInterval
+        timerInterval = setInterval(() => {
+            time++; //incremento el tiempo
+            drawPuzzle(); // vuelvo a dibujar el puzzle
+        }, 1000);
+        //llamo a drawPuzzle para mostrar el estado inicial 
+        drawPuzzle();
+
+    }
+
+    // -------------INICIO JUEGO--------------
 
     function startGame() {
         if (!selectedImage) return; // si no selecciono una imagen no hago nada
 
-        pieces = [];
         imageActual = new Image();
         imageActual.src = selectedImage;
 
         imageActual.onload = function () {
-            //calculo el ancho y alto de cada pieza
-            const pieceWidth = imageActual.width / cols;
-            const pieceHeight = imageActual.height / rows;
-            //recorro filas y columnas
-            for (let y = 0; y < rows; y++) {
-                for (let x = 0; x < cols; x++) {
-                    const canvasX = posStartX + x * pieceWidth;
-                    const canvasY = posStartY + y * pieceHeight;
-                    //por cada celda agrego un objeto pieza con sus coordenadas, tamaño, posicion en el canvas y rotacion.
-                    pieces.push({
-                        sourceX: x * pieceWidth,
-                        sourceY: y * pieceHeight,
-                        sourceWidth: pieceWidth,
-                        sourceHeight: pieceHeight,
-                        width: pieceWidth,
-                        height: pieceHeight,
-                        canvasX,
-                        canvasY,
-                        rotation: getRandomRotation()
-                    });
-                }
-            }
-            //marco el inicio del juego en true y reseteo el tiempo en cero
-            gameStarted = true;
-            time = 0;
-            //si ya habia un timerInterval lo limpio
-            if (timerInterval) {
-                clearInterval(timerInterval);
-            }
-            //creo un nuevo timerInterval
-            timerInterval = setInterval(() => {
-                time++; //incremento el tiempo
-                drawPuzzle(); // vuelvo a dibujar el puzzle
-            }, 1000);
-            //llamo a drawPuzzle para mostrar el estado inicial 
-            drawPuzzle();
+            // Aplicar filtro aleatorio a la imagen completa antes de cortar piezas
+            const filtros = [filtroBrillo, filtroGrices, filtroNegativo];
+            const filtroElegido = filtros[Math.floor(Math.random() * filtros.length)];
+
+            const tempCanvas = document.createElement("canvas");
+            const tempCtx = tempCanvas.getContext("2d");
+            tempCanvas.width = imageActual.width;
+            tempCanvas.height = imageActual.height;
+
+            tempCtx.drawImage(imageActual, 0, 0);
+            filtroElegido(tempCtx, 0, 0, imageActual.width, imageActual.height);
+
+            // Sobreescribimos imageActual con la versión filtrada
+            imageActual.src = tempCanvas.toDataURL();
+
+            imageActual.onload = function () {
+                createPice();
+            };
         };
-        //coloco el boton jugar de nuevo
-        playBtn = "Jugar de nuevo";
+
     }
 
-    // --- eventos ---
+    // -------------FUNCIONALIDAD CLICK----------------
 
-    //Calcula las coordenadas del click respecto al canvas (mouseX, mouseY) usando getBoundingClientRect().
-    canvas.addEventListener("mousedown", function (event) {
+    function onCanvasClick(event) {
         const rect = canvas.getBoundingClientRect();
         const mouseX = event.clientX - rect.left;
         const mouseY = event.clientY - rect.top;
 
-        // si el click cae dentro de las coordenadas de playButton
-        if ((mouseX >= playButton.x) && (mouseX <= playButton.x + playButton.width) &&
-            (mouseY >= playButton.y) && (mouseY <= playButton.y + playButton.height)) {
-            // Seleccionar imagen aleatoria e inicializo el juego
-            const randomIndex = Math.floor(Math.random() * thumbnails.length);
-            selectedImage = thumbnails[randomIndex].src;
-            startGame();
-            return;
-        }else if ((mouseX >= playAgain.x) && (mouseX <= playAgain.x + playAgain.width) &&
-            (mouseY >= playAgain.y) && (mouseY <= playAgain.y + playAgain.height)) {
-            // Seleccionar imagen aleatoria e inicializo el juego
-            const randomIndex = Math.floor(Math.random() * thumbnails.length);
-            selectedImage = thumbnails[randomIndex].src;
-            startGame();
-            return;
-        }
-
-        // Clic en una pieza
-        for (const piece of pieces) {
-            //recorro las piezas y si el click cae dentro de la caja canvasX..canvasX+width y canvasY..canvasY+height, entonces
-            if ((mouseX >= piece.canvasX) && (mouseX <= piece.canvasX + piece.width) &&
-                (mouseY >= piece.canvasY) && (mouseY <= piece.canvasY + piece.height)) {
-
-                if (event.button === 0) { // Botón izquierdo → rota la pieza -90°
-                    piece.rotation = (piece.rotation - 90 + 360) % 360; //asegura que la rotación quede en el rango 0..359
-                } else if (event.button === 2) { //derecho → rota la pieza 90°
-                    piece.rotation = (piece.rotation + 90) % 360;
+        // --- Si el juego está activo: solo se permite rotar piezas ---
+        if (gameStarted) {
+            for (const piece of pieces) {
+                if (
+                    mouseX >= piece.canvasX && mouseX <= piece.canvasX + piece.width &&
+                    mouseY >= piece.canvasY && mouseY <= piece.canvasY + piece.height
+                ) {
+                    if (event.button === 0) {
+                        piece.rotation = (piece.rotation - 90 + 360) % 360;
+                    } else if (event.button === 2) {
+                        piece.rotation = (piece.rotation + 90) % 360;
+                    }
+                    drawPuzzle();
+                    break;
                 }
-                drawPuzzle(); //actualizo la pantalla con la nueva rotacion
-                break;
             }
+            return; //Evita que se ejecute el resto del código
         }
-    });
 
+        // --- Si el juego NO está activo: solo se permite clic al botón ---
+        let clicked = false;
+        if (button.shape === "circle") {
+            const centerX = button.x + button.width / 2;
+            const centerY = button.y + button.height / 2;
+            const radius = Math.min(button.width, button.height) / 2;
+            const dx = mouseX - centerX;
+            const dy = mouseY - centerY;
+            clicked = dx * dx + dy * dy <= radius * radius;
+        } else if (button.shape === "rect") {
+            clicked =
+                mouseX >= button.x && mouseX <= button.x + button.width &&
+                mouseY >= button.y && mouseY <= button.y + button.height;
+        }
+
+        if (clicked) {
+            // Selecciona imagen aleatoria e inicia el juego
+            const randomIndex = Math.floor(Math.random() * thumbnails.length);
+            selectedImage = thumbnails[randomIndex].src;
+            startGame();
+        }
+    }
+    // -----------------EVENTOS---------------------
+    canvas.addEventListener("mousedown", onCanvasClick);
     // Evito el menú contextual en canvas al hacer clic derecho
     canvas.addEventListener("contextmenu", event => event.preventDefault());
 
-    // inicializacion, al cargar la página se dibuja la interfaz inicial (miniaturas, botón Jugar y tiempo en 0)
-    drawUI();
+    // -----------------FILTROS-----------------------
 
+    function filtroBrillo(ctx, iniX, iniY, w, h) {
 
+        const imageData = ctx.getImageData(iniX, iniY, w, h);
+        const data = imageData.data;
 
-    //       function aplicarFiltro(ctx, iniX, iniY, w, h) {
-    //         const imageData = ctx.getImageData(iniX,iniY, w, h);
-    //         const data = imageData.data;
-    //             for (let i = 0; i < data.length; i += 4) {
-    //             data[i] = 255 - data[i];
-    //             data[i + 1] = 255 - data[i + 1];
-    //             data[i + 2] = 255 - data[i + 2];
-    //             }
-    //         ctx.putImageData(imageData, iniX, iniY);
-    //   }
+        // aplicamos un 70 de reducion de color en cada uno de los valores
 
+        for (let i = 0; i < data.length; i += 4) {
+            // Ajustar cada componente de color (R, G, B)
+            data[i] = data[i] * 0.3;     // Rojo
+            data[i + 1] = data[i + 1] * 0.3; // Verde
+            data[i + 2] = data[i + 2] * 0.3;
+        }
 
+        ctx.putImageData(imageData, iniX, iniY);
 
-    // Aplica el filtro importado sobre la región de la pieza
-    function aplicarFiltro(ctx, iniX, iniY, w, h) {
-        // Selecciona aleatoriamente uno de los filtros importados
-        const filtros = [filtroBrillo, filtroGrices, filtroNegativo];
-        const filtro = filtros[Math.floor(Math.random() * filtros.length)];
-        filtro(ctx, iniX, iniY, w, h);
     }
 
+    function filtroSepia(ctx, iniX, iniY, w, h) {
+        const imageData = ctx.getImageData(iniX, iniY, w, h);
+        const data = imageData.data;
+
+        for (let i = 0; i < data.length - 1; i += 4) {
+
+            const rojo = data[i];
+            const green = data[i + 1];
+            const blue = data[i + 2];
+
+            //Calculo para tono sepia (Internet) supuestos valores.
+            const sepiaRed = ((rojo * 0.393) + (green * 0.769) + (blue * 0.189));
+            const sepiaGreen = ((rojo * 0.349) + (green * 0.686) + (blue * 0.168));
+            const sepiaBlue = ((rojo * 0.272) + (green * 0.534) + (blue * 0.131));
+
+            // Agrego tono por pixel para filtro sepia
+            data[i] = sepiaRed;
+            data[i + 1] = sepiaGreen;
+            data[i + 2] = sepiaBlue;
+        }
+        ctx.putImageData(imageData, iniX, iniY);
+    }
+
+    function filtroNegativo(ctx, iniX, iniY, w, h) {
+        const imageData = ctx.getImageData(iniX, iniY, w, h);
+        const data = imageData.data;
+
+        for (let i = 0; i < data.length - 1; i += 4) {
+
+            // Invertimos valores para dar negativo (maximo posible - valor)= valor invertido.
+            const red = data[i] = 255 - data[i];
+            const green = data[i + 1] = 255 - data[i + 1];
+            const blue = data[i + 2] = 255 - data[i + 2];
+
+            //Asignamos valores.
+            data[i] = red;
+            data[i + 1] = green;
+            data[i + 2] = blue;
+        }
+        ctx.putImageData(imageData, iniX, iniY);
+    }
+
+
+    function filtroGrices(ctx, iniX, iniY, w, h) {
+
+        const imageData = ctx.getImageData(iniX, iniY, w, h);
+        const data = imageData.data;
+
+        for (let i = 0; i < data.length - 1; i += 4) {
+
+            // Segun BT.601 calculamos los grices para mas espefiquidad
+            const avg = (data[i] * 0.299 + data[i + 1] * 0.587 + data[i + 2] * 0.114) / 3;
+            data[i] = avg;
+            data[i + 1] = avg;
+            data[i + 2] = avg;
+        }
+
+        ctx.putImageData(imageData, iniX, iniY);
+    }
+
+    // inicializacion, al cargar la página se dibuja la interfaz inicial (miniaturas, botón Jugar y tiempo en 0)
+    drawUI();
 });
