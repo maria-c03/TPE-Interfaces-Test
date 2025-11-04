3d1a1319febf9bc48c319c9104d0a70cb02caba2
Maria Cabrera <mariacabrera03@gmail.com>
2025-11-04 15:14:01 -0300

    Se elimina comentarios

--- TP4/js/peg/main.js
+++ TP4/js/peg/main.js
@@ -1,10 +1,7 @@
-// ----------INICIALIZACION DEL JUEGO-----------
 document.addEventListener("DOMContentLoaded", function () {
-    const pegModel = new PegModel(); // 1. MODELO
-
-    // Esperar a que el fondo del tablero cargue antes de crear la Vista y el Controlador
+    const pegModel = new PegModel(); 
     pegModel.backgroundImage.onload = () => {
-        const pegVista = new PegVista("canvasPeg", pegModel); // 2. VISTA
-        const pegController = new PegController(pegModel, pegVista); // 3. CONTROLADOR
+        const pegVista = new PegVista("canvasPeg", pegModel);
+        const pegController = new PegController(pegModel, pegVista);
     };
 });
